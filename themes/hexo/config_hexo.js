81f4393424a69d965ccff3b0b408d525c75c8d2f
Aniurm <85830317+ANIURM@users.noreply.github.com>
2023-05-08 19:18:58 +0800

    Update config_hexo.js

--- themes/hexo/config_hexo.js
+++ themes/hexo/config_hexo.js
@@ -1,6 +1,6 @@
 const CONFIG_HEXO = {
   HOME_BANNER_ENABLE: true,
-  HOME_BANNER_GREETINGS: ['Hi，我是一个程序员', 'Hi，我是一个打工人', 'Hi，我是一个干饭人', '欢迎来到我的博客🎉'], // 首页大图标语文字
+  HOME_BANNER_GREETINGS: ['我们原是自由飞翔的鸟', '飞去吧', '飞到那乌云背后明媚的山峦', '飞到那里', '到那蓝色的海角', '只有风在欢舞', '还有我相伴'], // 首页大图标语文字
 
   HOME_NAV_BUTTONS: true, // 首页是否显示分类大图标按钮
   HOME_NAV_BACKGROUND_IMG_FIXED: true, // 首页背景图滚动时是否固定，true 则滚动时图片不懂； false则随鼠标滚动
